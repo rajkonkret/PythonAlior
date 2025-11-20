95df68e426cbea35158cdce70102b0557f7afdd1
radek <radoslaw.janiak@softpos.eu>
2025-11-20 13:47:47 +0100

    [2 12] update zad2

--- zad2.py
+++ zad2.py
@@ -16,3 +16,16 @@ print(data)
 # Poznań           20         30          9
 # Warszawa          0         17         10
 # Kraków           10         10         10
+
+print(data.loc["Warszawa"] > 10)
+# product a    False
+# product b     True
+# product c    False
+# Name: Warszawa, dtype: bool
+
+import matplotlib.pyplot as plt
+# from matplotlib import pyplot as plt
+# pip install matplotlib
+
+data['product b'].plot(kind="hist", title='product b')
+plt.show()
